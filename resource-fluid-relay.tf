263ecb51a16246ef8f3c776a62d8213511f6b396
webstean@gmail.com <(Get-Item Env:UPN).Value>
2026-09-10 01:06:07 +1000

    Add Fluid Relay server resource configuration

--- resource-fluid-relay.tf
+++ resource-fluid-relay.tf
@@ -5,29 +5,27 @@ locals {
   fluid_name_location       = lower("${local.fluid_name}-${lower(var.location)}")
   fluid_random_suffix       = substr(random_string.environment.result, 0, 6)
   fluid_name_hostname       = lower(substr(replace("f${local.fluid_random_suffix}${local.fluid_name_location}", "-", ""), 0, 24))
+  fluid_sku                 = "basic"
 }
 
 resource "azurerm_fluid_relay_server" "this" {
-  name                = local.fluid_name
+  name                = local.fluid_name_hostname
   resource_group_name = module.environment_resource_group.resource.name
   location            = module.environment_resource_group.resource.location
 
-  storage_sku = "basic"
+  storage_sku = local.fluid_sku
   identity {
     type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.environment.id]
   }
 
   tags = { for key, value in module.environment_resource_group.resource.tags : key => value if lower(key) != "created" }
-  depends_on = [
-    module.environment_resource_group
-  ]
 }
 
 output "fluid_relay_service_id" {
   description = "The ID of the Fluid Relay service."
   sensitive   = false
-  value       = try(azurerm_fluid_relay_server.this.id), null)
+  value       = try(azurerm_fluid_relay_server.this.id, null)
 }
 
 output "fluid_relay_service_endpoints" {
