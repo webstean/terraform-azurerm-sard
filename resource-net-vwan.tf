0459671b6a34c499ffb9505ca8935ade34628d14
webstean@gmail.com <(Get-Item Env:UPN).Value>
2026-09-10 00:43:13 +1000

    Update virtual hub connection name generation for consistency

--- resource-net-vwan.tf
+++ resource-net-vwan.tf
@@ -29,7 +29,7 @@ resource "azurerm_virtual_hub" "this" {
 }
 
 resource "azurerm_virtual_hub_connection" "vnet" {
-  name                      = substr("link-vnet-${azurerm_virtual_network.this.name}-to-hub-${azurerm_virtual_hub.this[0].name}", 0, 80)
+  name                      = "link-vnet-${substr(md5("${azurerm_virtual_network.this.id}-${try(azurerm_virtual_hub.this[0].id, var.virtual_wan_hub_id)}"), 0, 16)}"
   virtual_hub_id            = try(azurerm_virtual_hub.this[0].id, var.virtual_wan_hub_id)
   remote_virtual_network_id = azurerm_virtual_network.this.id
 }
