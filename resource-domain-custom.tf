6e6438148f48c7519dab2466e744310c3f73c66c
webstean@gmail.com <(Get-Item Env:UPN).Value>
2026-09-14 10:35:35 +1000

    Update CAA record comments for Microsoft internal certificates

--- resource-domain-custom.tf
+++ resource-domain-custom.tf
@@ -43,8 +43,8 @@ locals {
   caa_records = [
     { flags = 0, tag = "issue", value = "letsencrypt.org" },
     { flags = 0, tag = "issuewild", value = "letsencrypt.org" },
-    { flags = 0, tag = "issue", value = "digicert.com" }, ## Microsoft needs this
-    { flags = 0, tag = "issuewild", value = "digicert.com" },
+    { flags = 0, tag = "issue", value = "digicert.com" },     ## Microsoft needs this for its internal certificates
+    { flags = 0, tag = "issuewild", value = "digicert.com" }, ## Microsoft needs this for its internal certificates
     # { flags = 0, tag = "issue",     value = "godaddy.com" },
     # { flags = 0, tag = "issuewild", value = "godaddy.com" },
     # { flags = 0, tag = "issue",     value = "pki.goog" },
